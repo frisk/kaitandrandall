b05cd348bc0e2292ad9ddb69c64e48bac69db35f
frisk <friskslalom6@Randalls-MacBook-Pro-2.local>
2014-03-21 17:52:20 -0700

    changed img width

--- js/nav-scroll.js
+++
@@ -1,8 +0,0 @@
-$(".scroll").click(function(event){		
-		event.preventDefault();
-		$('li.active').removeClass('active');
-		$(this).parent().addClass('active');
-		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 1000, function(){
-			$('#kait-randall').hide();	
-		});
-	});
